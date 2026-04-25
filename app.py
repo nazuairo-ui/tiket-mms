a12d29935c81d20a0d9898c0c87d31c2fa85140f
Naila <nazuairo@gmail.com>
2026-04-25 21:31:34 +0700

    161

--- app.py
+++ app.py
@@ -96,12 +96,12 @@ def load_user(user_id):
 
 def get_kuota():
     setting = Setting.query.filter_by(key='kuota').first()
-    return int(setting.value) if setting else 155
+    return int(setting.value) if setting else 161
 
 
 def get_limit_umum():
     setting = Setting.query.filter_by(key='limit_umum').first()
-    return int(setting.value) if setting else 85
+    return int(setting.value) if setting else 91
 
 
 def get_limit_MMS():
